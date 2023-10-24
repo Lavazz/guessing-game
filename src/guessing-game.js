d26c81bd76b4cf5141c4f82ffce867320201edf7
Katsiaryna_Kaloshych <katsiaryna_kaloshych@epam.com>
2023-10-24 15:57:07 +0300

    feat: add result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.res = Math.round((this.min + this.max)/2);
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
